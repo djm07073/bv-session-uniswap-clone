945136237ee21df8507de0cd31173d5139553613
suha jin <89185836+djm07073@users.noreply.github.com>
2023-10-08 14:25:08 +0900

    setting for session

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
-import dotenv from "dotenv";
 import { WagmiConfig } from "wagmi";
 import { polygon } from "wagmi/chains";
 import ConnectButton from "./components/ConnectButton";
-dotenv.config();
-const PID = process.env.PID!;
-// 1. Get projectId
-const projectId = PID;
+import Swap from "./components/Swap";
+import AddLiquidity from "./components/AddLiquidity";
+import WithdrawLiquidity from "./components/WithdrawLiquidity";
 
+// 1. Get projectId
+const projectId = "501269758a220a5479dec5ff5ecfdcf6";
 // 2. Create wagmiConfig
 const metadata = {
   name: "Web3Modal",
-  description: "Web3Modal Example",
+  description: "UniswapV2",
   url: "https://web3modal.com",
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
@@ -24,11 +24,24 @@ createWeb3Modal({ wagmiConfig, projectId, chains, defaultChain: polygon });
 
 export default function App() {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      {/* 지갑연결하기 */}
-      <ConnectButton />
-      {/* 스왑연동하기 */}
-      {/* 유동성공급/출금하기 */}
-    </WagmiConfig>
+    <div className="flex flex-col mt-5">
+      <WagmiConfig config={wagmiConfig}>
+        {/* 지갑연결하기 */}
+        <h2 className="font-bold text-2xl"> 1. Wallet Connect</h2>
+        <ConnectButton />
+        {/* 스왑연동하기 */}
+        <h2 className="font-bold text-2xl"> 2. Swap MATIC to USDC</h2>
+        <Swap />
+        {/* 유동성공급/출금하기 */}
+        <h2 className="font-bold text-2xl">
+          3. Add Liquidity on MATIC-USDC POOL
+        </h2>
+        <AddLiquidity />
+        <h2 className="font-bold text-2xl">
+          4. Withdraw Liquidity on MATIC-USDC POOL
+        </h2>
+        <WithdrawLiquidity />
+      </WagmiConfig>
+    </div>
   );
 }
---
+++ src/components/Swap.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+const ROUTER02 = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
+export default function Swap() {
+  return (
+    <div>
+      {/* MATIC 양 입력 , 받게 될 USDC 계산됨*/}
+
+      {/* Wrapping MATIC */}
+      {/* Approve WMATIC */}
+      {/* Swap */}
+    </div>
+  );
+}
